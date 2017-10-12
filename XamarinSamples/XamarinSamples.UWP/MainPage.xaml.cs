132e6caa15a5411d2303588c14c9802bb79ce9d1
ppn <ponponner@gmail.com>
2017-10-12 22:10:02 +0900

    追加 Xamarinのプロジェクトファイルを追加した。

---
+++ XamarinSamples/XamarinSamples.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace XamarinSamples.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new XamarinSamples.App());
+        }
+    }
+}
---
+++ XamarinSamples/XamarinSamples/Views/AboutPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+using Xamarin.Forms;
+
+namespace XamarinSamples.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
