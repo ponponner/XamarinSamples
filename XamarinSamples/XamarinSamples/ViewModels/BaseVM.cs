3ec2547cb53f854246d82d5f8dfd155d6291e971
ppn <ponponner@gmail.com>
2017-10-13 16:52:50 +0900

    追加 BaseModelの追加

---
+++ XamarinSamples/XamarinSamples/ViewModels/BaseVM.cs
@@ -0,0 +1,26 @@
+﻿using XamarinSamples.Helpers;
+using XamarinSamples.Models;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace XamarinSamples.ViewModels
+{
+    public class BaseVM : ObservableObject
+    {
+
+        #region Static fields and properties.
+        private static readonly PropertyChangedEventArgs PropertyChangedEventArgsOfIsBusy = new PropertyChangedEventArgs(nameof(IsBusy));
+        #endregion
+
+        #region Instance fields and properties.
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get => isBusy;
+            set => SetProperty(ref isBusy, value);
+        }
+        #endregion
+
+    }
+}
+
