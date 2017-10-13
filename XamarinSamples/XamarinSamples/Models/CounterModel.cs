cd34c5ec53c38e0ba8a6bd70b612e4a4762b2dc6
ppn <ponponner@gmail.com>
2017-10-13 16:14:28 +0900

    追加 CounterModelの追加

---
+++ XamarinSamples/XamarinSamples/Models/CounterModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XamarinSamples.Helpers;
+
+namespace XamarinSamples.Models
+{
+    public class CounterModel : ObservableObject
+    {
+
+        #region Instance fields and properties.
+        int _counter;
+        int _stepWidth;
+        public int Counter
+        {
+            get => _counter;
+            set => SetProperty(ref _counter, value);
+        }
+        public String CounterString
+        {
+            get => Counter.ToString();
+        }
+        public int CounterInvalid { private set; get; }
+        public int StepWidth
+        {
+            get => _stepWidth;
+            set => SetProperty(ref _stepWidth, value);
+        }
+        #endregion
+
+        #region Instance methods.
+
+        public CounterModel()
+        {
+            this.StepWidth = 1;
+            this.ResetCounter();
+        }
+        public void ResetCounter()
+        {
+            this.Counter = 0;
+            this.CounterInvalid = 0;
+        }
+        public void IncrementCounter()
+        {
+            this.Counter += this.StepWidth;
+            this.CounterInvalid += StepWidth;
+        }
+
+        #endregion
+
+    }
+}
